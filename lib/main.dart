7afd753b14bbb805ea9dd42cb8d6e8e4a08301ea
jewells07 <jewellsjoshi437@gmail.com>
2020-12-18 10:41:58 +0530

    5Scaffold-AppBar-Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-  runApp(MaterialApp(
-      home: Text('Flutter Demo'),
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First APP'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Hello world'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('click'),
+        ),
+      ),
     ));
