88e2f4decf10a3100d4a490e6c0332a61f672165
jewells07 <jewellsjoshi437@gmail.com>
2020-12-18 11:50:07 +0530

    8-Images-Assets

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,16 @@ class Home extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.red[600]),
       body: Center(
-        child: Text(
-          'hello world!',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
-        ),
+        // child: Image(
+        //   image: NetworkImage(
+        //       'https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8c3BhY2V8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'),
+        // ),
+        // child: Image.network('assets/space-1.jpg'),
+
+        // child: Image(
+        //   image: AssetImage('assets/space-2.jpg'),
+        // ),
+        child: Image.asset('assets/space-1.jpg'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
