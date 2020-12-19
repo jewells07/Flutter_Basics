be3e798e991b3b459a8eed35e23cd48fd0fb8503
jewells07 <jewellsjoshi437@gmail.com>
2020-12-19 18:45:50 +0530

    21-Functions-as-Parameters

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -3,7 +3,8 @@ import 'quote.dart';
 
 class QuoteCard extends StatelessWidget {
   final Quote quote;
-  QuoteCard({this.quote});
+  final Function delete;
+  QuoteCard({this.quote, this.delete});
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +30,12 @@ class QuoteCard extends StatelessWidget {
                   color: Colors.grey[800],
                 ),
               ),
+              SizedBox(height: 8.0),
+              FlatButton.icon(
+                onPressed: delete,
+                label: Text('delete quote'),
+                icon: Icon(Icons.delete),
+              )
             ],
           ),
         ));
